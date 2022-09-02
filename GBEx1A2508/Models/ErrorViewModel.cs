a371be6905fb90e89574230e2a2a1b287231cf37
Govanna Berato <99445044+GiovannaBerato@users.noreply.github.com>
2022-09-01 22:15:20 -0300

    Add files via upload

---
+++ GBEx1A2508/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GBEx1A2508.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ GBEx1A2508/Models/homemodel.cs
@@ -0,0 +1,19 @@
+﻿
+namespace GBEx1A2508.Models
+{
+    public class homemodel
+    {
+        public string Nome { get; set; }
+
+        public string email { get; set; }
+
+        public int telefone { get; set; }
+
+        public int id { get; set; }
+
+        public double salario { get; set; }
+
+        public int dataAdmissao { get; set; }
+
+    }
+}
